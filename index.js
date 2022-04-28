0cec4c6838be69df16cab8a3118f6d0ba26ca1d3
neeraj00890 <kumar74312@gmail.com>
2022-04-28 21:32:17 +0530

    fix: lib

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 alert("Hello, World!");
 //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
+// @neeraj00890:registry=https://npm.pkg.github.com
+// //npm.pkg.github.com/:_authToken=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_
